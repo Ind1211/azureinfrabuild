d7b35b556969d9ca8661bd445ba8f9cfb117a70a
Ind1211 <indrasen1211@gmail.com>
2026-02-26 15:51:45 +0530

    updated main

--- main.tf
+++ main.tf
@@ -40,19 +40,19 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "${var.vm_name}-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+#resource "azurerm_network_interface" "nic" {
+ # name                = "${var.vm_name}-nic"
+  #location            = azurerm_resource_group.rg.location
+  #resource_group_name = azurerm_resource_group.rg.name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
+  #ip_configuration {
+   # name                          = "internal"
+    #subnet_id                     = azurerm_subnet.subnet.id
+    #private_ip_address_allocation = "Dynamic"
     # Remove public IP association
     # public_ip_address_id = null
-  }
-}
+  #}
+#}
 
 
 resource "azurerm_network_security_group" "nsg" {
@@ -73,18 +73,18 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
-#resource "azurerm_network_interface" "nic" {
- # name                = "${var.vm_name}-nic"
-  #location            = azurerm_resource_group.rg.location
-  #resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.vm_name}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-  #ip_configuration {
-   # name                          = "internal"
-    #subnet_id                     = azurerm_subnet.subnet.id
-    #private_ip_address_allocation = "Dynamic"
-    #public_ip_address_id          = var.create_public_ip ? azurerm_public_ip.public_ip[0].id : null
-  #}
-#}
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = var.create_public_ip ? azurerm_public_ip.public_ip[0].id : null
+  }
+}
 
 resource "azurerm_network_interface_security_group_association" "assoc" {
   network_interface_id      = azurerm_network_interface.nic.id
