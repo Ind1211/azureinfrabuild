2754aad3ca17f7a50075bb971cc81e29175eddc1
Ind1211 <indrasen1211@gmail.com>
2026-03-09 14:17:49 +0530

    updated root module config

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
 provider "azurerm" {
-
+  features {}
 }
-module "vm_instance" {
-  source = "./main.tf"
+module "vm_deploy" {
+  source = "./modules/vm_deploy"
+    ARM_CLIENT_ID = var.ARM_CLIENT_ID
+    ARM_CLIENT_SECRET = var.ARM_CLIENT_SECRET
+    ARM_SUBSCRIPTION_ID = var.ARM_SUBSCRIPTION_ID
+    ARM_TENANT_ID = var.ARM_TENANT_ID
+    azurevm_rg = var.azurevm_rg
+  
 }
 
